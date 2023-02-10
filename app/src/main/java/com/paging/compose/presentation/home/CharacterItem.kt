1fd04a174894afb1bba43a968b3f522b76f336bb
Darwin <iamdarwin.salcedo@gmail.com>
2023-02-10 14:41:01 -0300

    Image

--- app/src/main/java/com/paging/compose/presentation/home/CharacterItem.kt
+++ app/src/main/java/com/paging/compose/presentation/home/CharacterItem.kt
@@ -11,7 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.paging.compose.domain.model.Character
 
 
@@ -48,6 +52,16 @@ fun ImageCard(url: String) {
     Box(modifier = Modifier
         .height(200.dp)
         .fillMaxWidth()) {
+
+        AsyncImage(model = ImageRequest.Builder(LocalContext.current)
+            .data(url)
+            .crossfade(1000)
+            .build(),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
 
     }
 }
