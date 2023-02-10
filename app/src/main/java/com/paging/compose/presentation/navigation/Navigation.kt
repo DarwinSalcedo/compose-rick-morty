3f4cb0858db1909e3d2b3098e6304344d2737498
Darwin <iamdarwin.salcedo@gmail.com>
2023-02-10 14:22:45 -0300

    set navigation

---
+++ app/src/main/java/com/paging/compose/presentation/navigation/Navigation.kt
@@ -0,0 +1,51 @@
+package com.paging.compose.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.paging.compose.domain.model.Character
+import com.paging.compose.presentation.home.HomeScreen
+import com.paging.compose.presentation.home.HomeViewModel
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController,
+        startDestination = NavItem.Main.route) {
+
+        composable(NavItem.Main) {
+            HomeScreen(
+                onItemClick = {
+                    navController.currentBackStackEntry?.savedStateHandle?.set("_data", it)
+                    navController.navigate(NavItem.Detail.createRoute(it.id.toInt()))
+                }, hiltViewModel<HomeViewModel>()
+            )
+        }
+        composable(NavItem.Detail) {
+            val show =
+                navController.previousBackStackEntry?.savedStateHandle?.get<Character>("_data")
+            show?.let {
+                /*  DetailScreen(show = it,
+                      onUpClick = { navController.popBackStack() })*/
+            }
+        }
+
+    }
+}
+
+private fun NavGraphBuilder.composable(
+    navItem: NavItem,
+    content: @Composable (NavBackStackEntry) -> Unit,
+) {
+    composable(
+        route = navItem.route,
+        arguments = navItem.args
+    ) {
+        content(it)
+    }
+}
